6f36ec99a14bdd3dde59486149d92b29ba23234e
Mayur1011 <mayurpokharkar6@gmail.com>
2025-06-18 23:06:27 +0530

    temp

---
+++ .config/hyprland_dots/hypr/scripts/hyprsunset.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Kill current hyprsunset session
+killall -q hyprsunset
+
+# Get the current hour (in 24-hour format)
+current_hour=$(date +%H)
+echo "$current_hour"
+
+# If it’s 6pm to 7am, hyprsunset -t 5000
+# Else hyprsunset -t 6500
+if [ "$current_hour" -ge 20 ] || [ "$current_hour" -le 6 ]; then
+    echo "Changing to nighttime hyprsunset"
+    hyprsunset -t 4000 &
+else
+    echo "Changing to default hyprsunset"
+    hyprsunset -t 6500 &
+fi
+
